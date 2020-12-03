31a08a185c443f0ad4b8d8bdbe7e86b10d0cf6b5
Oriol <oriolalascercos@gmail.com>
2020-12-03 17:12:18 +0100

    day1

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -1,20 +1,28 @@
 use std::env;
 use std::fs;
 use itertools::Itertools;
+use reduce::Reduce;
 
 fn main() {
 
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: ./day1 <filename>");
+    if args.len() < 3 {
+        println!("Usage: ./day1 <permutations> <filename>");
         std::process::exit(1);
     }
-    let contents: String = fs::read_to_string(&args[1])
+    let p = args[1].parse::<i32>();
+    let contents: String = fs::read_to_string(&args[2])
         .expect("File not found");
     let words: Vec<&str> = contents.split_whitespace().collect();
-    let integers: Vec<i32> = words.iter().map(|x| x.parse::<i32>().unwrap()).rev().collect();
-    let perms: Vec<Vec<i32>> = integers.into_iter().permutations(2).filter(|x| x[0] + x[1] == 2020).collect();
-    let solution: Vec<i32> = perms[0].clone();
-    println!("{}", solution[0] * solution[1]);
-
+    let integers: Vec<i32> = words.iter()
+        .map(|x| x.parse::<i32>().unwrap()).rev().collect();
+    let perms: Option<Vec<i32>> = integers.into_iter()
+        .permutations(p)
+        .filter(|x| x.into_iter().sum::<i32>() == 2020).next();
+    match perms {
+        Some(x) => {
+            println!("{}", x.into_iter().reduce(|a, b| a * b).unwrap());
+        },
+        None => println!("Not found"),
+    }
 }
