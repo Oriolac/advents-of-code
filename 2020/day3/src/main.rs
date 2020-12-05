64af6f0e54358bd7ac00922e6593532cb2762034
Oriol Alàs <oriolalascercos@gmail.com>
2020-12-05 23:13:02 +0100

    Refactor player OOP

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -16,6 +16,36 @@ fn parse(line: String) -> Vec<Tile> {
         .collect();
 }
 
+struct Player {
+    x: usize,
+    y: usize,
+    trees: usize,
+}
+
+impl Player {
+    fn move_to(&mut self, worldmap: Vec<Vec<Tile>>) {
+        let module = worldmap[0].len();
+        self.x = (self.x + 3) % module;
+        self.y += 1;
+        self.trees += match worldmap[self.y][self.x] {
+            Tree() => 1,
+            Nothing() => 0,
+        };
+    }
+
+    fn has_finished(&mut self, size: usize) -> bool {
+        return self.y != size - 1;
+    }
+}
+
+fn trees_part_1(worldmap: Vec<Vec<Tile>>) -> usize {
+    let mut player = Player { x: 0, y: 0, trees: 0 };
+    while player.has_finished(worldmap.len()) {
+        player.move_to(worldmap.clone());
+    }
+    player.trees
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file: String = fs::read_to_string(&args[1])
@@ -24,17 +54,5 @@ fn main() {
         .split('\n')
         .map(|x| parse(String::from(x)))
         .collect();
-    let mut x: usize = 0;
-    let mut y: usize = 0;
-    let mut trees: usize = 0;
-    let module = worldmap[0].len();
-    while y != worldmap.len() - 1 {
-        x = (x + 3) % module;
-        y += 1;
-        trees += match worldmap[y][x] {
-            Tree() => 1,
-            Nothing() => 0,
-        };
-    }
-    println!("{}", trees)
+    println!("{}", trees_part_1(worldmap))
 }
